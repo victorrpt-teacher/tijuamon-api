e661f2e4eb612cc1e6793deb8010be1298dd97e8
Victor Perez <victor.rpt22@gmail.com>
2025-09-20 16:38:24 -0700

    Agregue nuevos tijuamones

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ tijuamones = [
     {"id": 1, "name": "Caguamon", "type": "Water", "level": 5, "hp": 30, "attack": 15, "defense": 10, "habilities": ["Bottle Thrower", "Smell Attack"]},
     {"id": 2, "name": "Choloko", "type": "Fire", "level": 7, "hp": 40, "attack": 12, "defense": 14, "habilities": ["Smoke", "Knife Thrower"]},
     {"id": 3, "name": "Canalin", "type": "Dirt", "level": 6, "hp": 35, "attack": 14, "defense": 12, "habilities": ["Poisoned Water Thrower", "Mud Attack"]},
+    {"id": 4, "name": "BurroCebra", "type": "Normal", "level": 8, "hp": 45, "attack": 18, "defense": 11, "habilities": ["Tourist Trap", "Slow Attack"]},
+    {"id": 5, "name": "Mimoy", "type": "Psych", "level": 10, "hp": 50, "attack": 20, "defense": 15, "habilities": ["Mind Control", "Hypnosis"]},
+    {"id": 6, "name": "Maguana", "type": "Rock", "level": 12, "hp": 60, "attack": 25, "defense": 20, "habilities": ["Rock Throw", "Growl"]},
+    {"id": 7, "name": "Policon", "type": "Electric", "level": 15, "hp": 70, "attack": 30, "defense": 25, "habilities": ["Bite", "Ticketing"]}
 ]
 
 @app.route('/', methods=['GET'])
